99d0e359cc4f5b528b1a00a804ae8f15b228ff72
Tuire Peurala <tuire.huhtamaki@helsinki.fi>
2013-03-05 12:01:37 +0200

    Async reading of players file -> synchronous

--- public_html/Server/AgentEx-server.js
+++ public_html/Server/AgentEx-server.js
@@ -11,99 +11,101 @@ var url = require('url');
 
 
 function getContentType(extname) {
-	switch (extname) {
-	case '.js':
-		return 'text/javascript';
-	case '.css':
-		return 'text/css';
-	case '.jpg':
-	case '.jpeg':
-		return 'image/jpeg';
-	case '.png':
-		return 'image/png';
-	default:
-		return 'text/html';
-	}
+    switch (extname) {
+    case '.js':
+        return 'text/javascript';
+    case '.css':
+        return 'text/css';
+    case '.jpg':
+    case '.jpeg':
+        return 'image/jpeg';
+    case '.png':
+        return 'image/png';
+    default:
+        return 'text/html';
+    }
 }
 
 function serveFile(filePath, response) {
-	console.log("LOG: looking for file '" + filePath + "'");
+    console.log("LOG: looking for file '" + filePath + "'");
+    var extname, contentType;
+    extname = path.extname(filePath);
+    contentType = getContentType(extname);
 
-	var extname = path.extname(filePath);
-	var contentType = getContentType(extname);
-
-	path.exists(filePath, function (exists) {
-		if (exists) {
-			fs.readFile(filePath, function (error, content) {
-				if (error) {
-					response.writeHead(500);
-					response.end();
-				} else {
-					response.writeHead(200, { 'Content-Type': contentType });
-					response.end(content, 'utf-8');
-				}
-			});
-		} else {
-			response.writeHead(404);
-			response.write("No page '" + filePath + "'");
-			response.end();
-		}
-	});
-}
-function readJSON(callback, parameters) {
-    fs = require('fs');
-    console.log("reading");
-    var file = 'players.json';
-        fs.readFile(file, 'utf8', function (err, data) {
-            if (err) {
-                console.log('Error: ' + err);
-                return err;
-            }
-      callback(JSON.parse(data), parameters);  
+    path.exists(filePath, function (exists) {
+        if (exists) {
+            fs.readFile(filePath, function (error, content) {
+                if (error) {
+                    response.writeHead(500);
+                    response.end();
+                } else {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                }
+            });
+        } else {
+            response.writeHead(404);
+            response.write("No page '" + filePath + "'");
+            response.end();
+        }
     });
-          
 }
 
-function saveJSON(object){
-     fs = require('fs');
+function readPlayers() {
+    var playersText;
+    playersText = fs.readFileSync('players.json', 'utf8');
+    return JSON.parse(playersText);
+}
+
+function savePlayers(object) {
     console.log("saving");
     fs.writeFile('players.json', JSON.stringify(object), function (err) {
-        if (err) return console.log(err.description);
-
+        if (err) {
+            return console.log(err.description);
+        }
     });
 }
 
+function isValidNewUser(name, pwd, players) {
+    if (name !== undefined && pwd !== undefined) {
+        return true;
+    } //TODO check player name is unique
+}
 
-function newUser(players, post) {
-        console.log("newUser");
-        var players;
-        if (post.name !== undefined && post.pwd1 !== undefined) {
-            var newUser = {"name": post.name, "pwd":post.pwd1};
-            players.push(newUser);
-            saveJSON(players);
-        }
-             
-            
-	//TODO saving, generate&return client id to store on localstore
+function generateId(players) {
+    var id = players.length + 1; //TODO
+    console.log("ID " + id);
+    return id;
+}
 
-        //console.log(players.name)
-       
-        console.log("loppu");
-    
-	return true;
+function addNewUser(players, post) {
+    var newPlayer, id;
+    id = '';
+    console.log("newUser");
+    if (isValidNewUser(post.name, post.pwd1, players)) {
+        id = generateId(players);
+        newPlayer = {"name": post.name, "pwd": post.pwd1, "id": id};
+        players.push(newPlayer);
+        savePlayers(players);
+    }
+    console.log("loppu");
+    return id;
 }
 
 function register(post, response) {
-	var success = true;
-	if (post !== undefined) {
-            readJSON(newUser, post);
-	}
-	if (success) {
-		response.writeHead(301, { 'Location': 'main.html'});
-	    response.end();
-	} else {
-		serveFile('../Client/index.html', response);
-	}
+    var players, id;
+    id = '';
+    if (post !== undefined) {
+        players = readPlayers();
+        id = addNewUser(players, post);
+    }
+    if (id !== '') {
+        //TODO modify html before returning
+        response.writeHead(301, { 'Location': 'main.html'});
+        response.end();
+    } else {
+        serveFile('../Client/index.html', response);
+    }
 }
 
 function loginWithId(clientId) {
@@ -115,81 +117,61 @@ function loginWithPwd(name, pwd) {
 }
 
 function login(post, response) {
-	var authorized = false;
-	if (post !== undefined) {
-		if (post.clientId !== undefined) {
-			authorized = loginWithId(post.clientId);
-		} else if (post.name !== undefined && post.pwd !== undefined) {
-			authorized = loginWithPwd(post.name, post.pwd);
-		}
-	}
+    var authorized = false;
+    if (post !== undefined) {
+        if (post.clientId !== undefined) {
+            authorized = loginWithId(post.clientId);
+        } else if (post.name !== undefined && post.pwd !== undefined) {
+            authorized = loginWithPwd(post.name, post.pwd);
+        }
+    }
 
-	if (authorized) {
-		response.writeHead(301, { 'Location': 'main.html'});
-	    response.end();
-	} else {
-		serveFile('../Client/index.html', response);
-	}
+    if (authorized) {
+        response.writeHead(301, { 'Location': 'main.html'});
+        response.end();
+    } else {
+        serveFile('../Client/index.html', response);
+    }
 }
 
 function routeRequest(path, response, getData, postData) {
-	var filePath;
-	filePath = '../Client';
-	console.log('register hit 0');
-	switch (path) {
-	case '/':
-		return serveFile(filePath + '/index.html', response);
-	case '/Register':
-		return register(postData, response);
-	case '/Login':
-		return login(postData, response);
-	default:
-		return serveFile(filePath + path, response);
-	}
+    var filePath;
+    filePath = '../Client';
+    console.log('register hit 0');
+    switch (path) {
+    case '/':
+        return serveFile(filePath + '/index.html', response);
+    case '/Register':
+        return register(postData, response);
+    case '/Login':
+        return login(postData, response);
+    default:
+        return serveFile(filePath + path, response);
+    }
 }
 
 var server = http.createServer(function (request, response) {
-	console.log("LOG: requesting: '" + request.url + "'");
+    console.log("LOG: requesting: '" + request.url + "'");
 
-	var getData, postData, urlData, filePath;
-	urlData = url.parse(request.url, true);
-	getData = urlData.query;
-	//console.log("Parsed: "+ urlData.pathname);
-	//console.log("Data", JSON.stringify(queryData));
-
-	/* Tuire: Never used?!?! 
-	if (urlData.pathname === "friendfind.html") {
-		var user = request.url.substring(22,request.url.indexOf("&"));
-                player_table[0].name = user;
-		var startingOrientation = request.url.substring(request.url.indexOf("&")+13,request.url.length);
-		//console.log("user: '"+user +"'. orientation: '" + startingOrientation +"'");
-
-		if (startingOrientation == "map")
-			filePath =  '/map.html';
-		else
-			filePath = '/webcam.html';
-
-		//301 moved permanently
-		//302 temporary redirect
-		response.writeHead(301, { 'Location': filePath });
-	        response.end();
-
-		return;
-	}*/
-	if (request.method === 'POST') {
-		var querystring = require('querystring');
-		var data = '';
-		request.on('data', function (chunk) {
-			data += chunk;
-		});
-		request.on('end', function () {
-			postData = querystring.parse(data);
-			//routing called after all POST data is read
-			routeRequest(urlData.pathname, response, getData, postData);
-		});
-	} else { //GET
-		routeRequest(urlData.pathname, response, getData, postData);
-	}
+    var getData, postData, urlData, filePath, querystring, data;
+    urlData = url.parse(request.url, true);
+    getData = urlData.query;
+    //console.log("Parsed: "+ urlData.pathname);
+    //console.log("Data", JSON.stringify(queryData));
+    if (request.method === 'POST') {
+        querystring = require('querystring');
+        data = '';
+        request.on('data', function (chunk) {
+            data += chunk;
+        });
+        request.on('end', function () {
+            postData = querystring.parse(data);
+            //routing called after all POST data is read
+            routeRequest(urlData.pathname, response, getData, postData);
+        });
+    } else { //GET
+        routeRequest(urlData.pathname, response, getData, postData);
+    }
 });
 
 server.listen(PORT);
